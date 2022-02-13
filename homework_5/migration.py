e46250931a2a4e79eecbba34435f535fb07314aa
Alexander <psionickmobile@gmail.com>
2022-02-13 13:29:01 +0200

    updated HW5

--- homework_5/migration.py
+++ homework_5/migration.py
@@ -7,9 +7,33 @@ class Migration:
         self.conn_string_source = conn_string_source
         self.conn_string_target = conn_string_target
         self.result_folder = result_folder
+        self.conn_source = None
+        self.conn_target = None
+
+    def connect_source(self):
+        self.conn_source = psycopg2.connect(self.conn_string_source)
+
+    def close_source_conn(self):
+        self.conn_source.close()
+
+    def close_target_conn(self):
+        self.conn_target.close()
+
+    def __check_target_connection(self):
+        if self.conn_target is None:
+            self.connect_target()
+
+    def __check_source_connection(self):
+        if self.conn_source is None:
+            self.connect_source()
+
+    def connect_target(self):
+        self.conn_target = psycopg2.connect(self.conn_string_target)
 
     def download_data_by_tables(self, tables):
-        with psycopg2.connect(self.conn_string_source) as conn, conn.cursor() as cursor:
+        self.__check_source_connection()
+
+        with self.conn_source.cursor() as cursor:
             for table in tables:
                 q = f"COPY {table} TO STDOUT WITH DELIMITER ',' CSV HEADER;"
                 filename = f"./{self.result_folder}/{table}.csv"
@@ -17,27 +41,29 @@ class Migration:
                 with open(filename, 'w', encoding='utf-8') as f:
                     cursor.copy_expert(q, f)
 
-
     def load_data_by_tables(self, tables):
-        with psycopg2.connect(self.conn_string_target) as conn, conn.cursor() as cursor:
+        self.__check_target_connection()
+
+        with self.conn_target.cursor() as cursor:
             for table in tables:
                 q = f"COPY {table} from STDIN WITH DELIMITER ',' CSV HEADER;"
                 filename = f"./{self.result_folder}/{table}.csv"
                 with open(filename, 'r', encoding='utf-8') as f:
                     cursor.copy_expert(q, f)
 
-
     def check_loaded_data_by_tables(self, tables):
-        with psycopg2.connect(self.conn_string_source) as conn, conn.cursor() as cursor:
-                print("-----Source Database-----")
-                for table in tables:
-                    cursor.execute(f'select count(*) from {table} limit 1')
-                    count = cursor.fetchall()
-                    print(f"{table} count: {count}")
+        self.__check_source_connection()
+        with self.conn_source.cursor() as cursor:
+            print("-----Source Database-----")
+            for table in tables:
+                cursor.execute(f'select count(*) from {table}')
+                count = cursor.fetchall()
+                print(f"{table} count: {count}")
 
-        with psycopg2.connect(self.conn_string_target) as conn, conn.cursor() as cursor:
+        self.__check_target_connection()
+        with self.conn_target.cursor() as cursor:
             print("-----Target Database-----")
             for table in tables:
-                cursor.execute(f'select count(*) from {table} limit 1')
+                cursor.execute(f'select count(*) from {table}')
                 count = cursor.fetchall()
                 print(f"{table} count: {count}")
